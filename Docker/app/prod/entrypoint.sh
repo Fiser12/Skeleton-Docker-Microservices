0fa38574d11bc2dc0e4cf427a7939a64e3a0b642
Fiser <fiser12@icloud.com>
2018-09-30 22:27:56 +0200

    Finished the update of the microservice to 4.1 version

--- Docker/app/prod/entrypoint.sh
+++ Docker/app/prod/entrypoint.sh
@@ -1,19 +1,4 @@
 sed \
-        -e "s/\${database_host}/${DB_HOST}/" \
-        -e "s/\${database_port}/${DB_PORT}/" \
-        -e "s/\${database_name}/${DB_DATABASE}/" \
-        -e "s/\${database_user}/${DB_ROOT}/" \
-        -e "s/\${database_password}/${MYSQL_ROOT_PASSWORD}/" \
-        -e "s/\${database_server_version}/${MYSQL_DATABASE_SERVER_VERSION}/" \
-        -e "s/\${mailer_transport}/${MAILER_TRANSPORT}/" \
-        -e "s/\${mailer_host}/${MAILER_HOST}/" \
-        -e "s/\${mailer_user}/${MAILER_USER}/" \
-        -e "s/\${mailer_password}/${MAILER_PASSWORD}/" \
-        -e "s/\${mail_delivery_address}/${MAILER_DELIVERY_ADDRESS}/" \
-        -e "s/\${router_request_context_host}/${REQUEST_CONTEXT_HOST}/" \
-        -e "s/\${router_request_context_scheme}/${REQUEST_CONTEXT_SCHEME}/" \
-        -e "s/\${secret-app}/${SECRET_KEY_APP}/" \
-        -e "s/\${secret-compositeui}/${SECRET_KEY_COMPOSITE_UI}/" \
-        -e "s/\${env}/prod/" \
-        /app/App/env-parameters.yml  > /app/App/parameters.yml
+        -e "s/\${APP_ENV}/${APP_ENV}/" \
+        /app/App/.env-docker.dist  > /app/App/.env
 exec "$@"
