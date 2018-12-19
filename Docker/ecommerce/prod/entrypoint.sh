bbeb41623afea757cc8ab5cf4d6f91121357b1c0
Fiser <fiser12@icloud.com>
2018-12-19 22:30:35 +0100

    FEAT Changes for new database

--- Docker/ecommerce/prod/entrypoint.sh
+++ Docker/ecommerce/prod/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-php /app/Ecommerce/etc/bin/symfony-console doctrine:database:create --if-not-exists
-php /app/Ecommerce/etc/bin/symfony-console do:mi:mi -v --no-interaction --allow-no-migration
-openssl genrsa -out /app/Ecommerce/var/jwt/private.pem -aes256 -passout pass:${JWT_PASSPHRASE} 4096
-openssl rsa -pubout -in /app/Ecommerce/var/jwt/private.pem -out /app/Ecommerce/var/jwt/public.pem -passin pass:${JWT_PASSPHRASE}
+php /app/ECommerce/etc/bin/symfony-console doctrine:database:create --if-not-exists
+php /app/ECommerce/etc/bin/symfony-console do:mi:mi -v --no-interaction --allow-no-migration
+openssl genrsa -out /app/ECommerce/var/jwt/private.pem -aes256 -passout pass:${JWT_PASSPHRASE} 4096
+openssl rsa -pubout -in /app/ECommerce/var/jwt/private.pem -out /app/ECommerce/var/jwt/public.pem -passin pass:${JWT_PASSPHRASE}
 
 exec "$@"
