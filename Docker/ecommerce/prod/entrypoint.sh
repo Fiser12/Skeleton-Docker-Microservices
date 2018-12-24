e63b136fa7304b71aab835153f5c4ecedac699d5
Fiser <fiser12@icloud.com>
2018-12-24 04:52:11 +0100

    FIX Errors in production solved

--- Docker/ecommerce/prod/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-php /app/ECommerce/etc/bin/symfony-console doctrine:database:create --if-not-exists
-php /app/ECommerce/etc/bin/symfony-console do:mi:mi -v --no-interaction --allow-no-migration
-openssl genrsa -out /app/ECommerce/var/jwt/private.pem -aes256 -passout pass:${ECOMMERCE_JWT_PASSPHRASE} 4096
-openssl rsa -pubout -in /app/ECommerce/var/jwt/private.pem -out /app/ECommerce/var/jwt/public.pem -passin pass:${ECOMMERCE_JWT_PASSPHRASE}
-
-exec "$@"
